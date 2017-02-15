67cb643dfe5629a6d33d2179537e5880d1598da3
Qwacks <jared.j@eagles.ewu.edu>
2017-02-14 21:04:06 -0800

    getting pc files up to date
    was having issues downloading

--- Movie-Trailer/js/scripts.js
+++ Movie-Trailer/js/scripts.js
@@ -1,2 +1,5 @@
-// My Scripts
+// Lettering
 $('h1').lettering();
+
+// Video Player
+$('video').mediaelementplayer();
--- README.md
+++
@@ -1,8 +0,0 @@
-# I'm not sure
-
-## Content Priority Checklist
-
-1. Trailer
-2. Title
-3. Release
-4. Buy Tickets
